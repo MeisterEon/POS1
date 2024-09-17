688c6dcee0f1842aa96137b21416f8989c6a67dd
AdlesgruberS22054 <adlesgrubers22054@sus.htl-grieskirchen.at>
2024-09-17 12:11:33 +0200

    Added algorithm

--- EratosthenesPrimeSieve/src/net/htlgkr/adlesgrubers22054/pos2/primechecker/EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieve/src/net/htlgkr/adlesgrubers22054/pos2/primechecker/EratosthenesPrimeSieve.java
@@ -1,23 +1,31 @@
 package net.htlgkr.adlesgrubers22054.pos2.primechecker;
 
-import java.util.List;
-
 public class EratosthenesPrimeSieve implements PrimeSieve{
-    private int limit;
-    private List<Integer> numberList;
+    private Boolean[] primeNumberArray;
 
     public EratosthenesPrimeSieve(int limit) {
-        this.limit = limit;
+        primeNumberArray = new Boolean[limit];
+        for (int i = 2; i < limit; i++) {
+            primeNumberArray[i] = true;
+        }
+
+        for (int i = 2; i < Math.sqrt(limit); i++) {
+            if (primeNumberArray[i]) {
+                for (j = i * i; i < limit; i += i) {
+                    primeNumberArray[j] = false;
+                }
+            }
+        }
     }
 
     @Override
     public boolean isPrime(int p) {
-        return numberList.contains(p);
+        return primeNumberArray.contains(p);
     }
 
     @Override
     public void printPrimes() {
-        for (Integer i : numberList) {
+        for (Integer i : primeNumberArray) {
             System.out.println(i);
         }
     }
