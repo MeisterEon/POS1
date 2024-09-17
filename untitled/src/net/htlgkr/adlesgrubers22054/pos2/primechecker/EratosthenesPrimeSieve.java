88238740427f0156d5e0533da7afb91e3a5ee707
AdlesgruberS22054 <adlesgrubers22054@sus.htl-grieskirchen.at>
2024-09-17 11:12:48 +0200

    Setup

---
+++ untitled/src/net/htlgkr/adlesgrubers22054/pos2/primechecker/EratosthenesPrimeSieve.java
@@ -0,0 +1,14 @@
+package net.htlgkr.adlesgrubers22054.pos2.primechecker;
+
+public class EratosthenesPrimeSieve implements PrimeSieve{
+
+    @Override
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
+}
---
+++ untitled/src/net/htlgkr/adlesgrubers22054/pos2/primechecker/PrimeSieve.java
@@ -0,0 +1,8 @@
+package net.htlgkr.adlesgrubers22054.pos2.primechecker;
+
+public interface PrimeSieve {
+    // U e b e r p r u ef t ob p ei n e Primzahl i s t ode r ni c h t .
+    boolean isPrime(int p) ;
+    // Gibt a l l e Primzahlen b i s zu r Obergrenze auf de r Konsole aus .
+    void printPrimes() ;
+}
