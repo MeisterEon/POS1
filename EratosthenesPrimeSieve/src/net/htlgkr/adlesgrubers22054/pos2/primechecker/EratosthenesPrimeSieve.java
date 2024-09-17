0f101220207bb5fc7daca80dd8bdb47056ec3530
AdlesgruberS22054 <adlesgrubers22054@sus.htl-grieskirchen.at>
2024-09-17 12:32:27 +0200

    Tested and fixed project

--- EratosthenesPrimeSieve/src/net/htlgkr/adlesgrubers22054/pos2/primechecker/EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieve/src/net/htlgkr/adlesgrubers22054/pos2/primechecker/EratosthenesPrimeSieve.java
@@ -4,14 +4,14 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
     private final Boolean[] primeNumberArray;
 
     public EratosthenesPrimeSieve(int limit) {
-        primeNumberArray = new Boolean[limit];
-        for (int i = 2; i < limit; i++) {
+        primeNumberArray = new Boolean[limit + 1];
+        for (int i = 2; i < limit + 1; i++) {
             primeNumberArray[i] = true;
         }
 
-        for (int i = 2; i < Math.sqrt(limit); i++) {
+        for (int i = 2; i <= Math.sqrt(limit); i++) {
             if (primeNumberArray[i]) {
-                for (int j = i * i; i < limit; i += i) {
+                for (int j = i * i; j < limit + 1; j += i) {
                     primeNumberArray[j] = false;
                 }
             }
---
+++ EratosthenesPrimeSieve/src/net/htlgkr/adlesgrubers22054/pos2/primechecker/Main.java
@@ -0,0 +1,8 @@
+package net.htlgkr.adlesgrubers22054.pos2.primechecker;
+
+public class Main {
+    public static void main(String[] args) {
+        EratosthenesPrimeSieve eratosthenesPrimeSieve = new EratosthenesPrimeSieve(1000);
+        eratosthenesPrimeSieve.printPrimes();
+    }
+}
