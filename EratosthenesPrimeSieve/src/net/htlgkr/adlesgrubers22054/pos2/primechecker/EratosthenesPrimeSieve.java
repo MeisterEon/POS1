015969e86a35a10aa1516cf178c80057006448ae
AdlesgruberS22054 <adlesgrubers22054@sus.htl-grieskirchen.at>
2024-09-17 11:41:27 +0200

    Varibles and methods added

--- EratosthenesPrimeSieve/src/net/htlgkr/adlesgrubers22054/pos2/primechecker/EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieve/src/net/htlgkr/adlesgrubers22054/pos2/primechecker/EratosthenesPrimeSieve.java
@@ -1,6 +1,15 @@
 package net.htlgkr.adlesgrubers22054.pos2.primechecker;
 
+import java.util.List;
+
 public class EratosthenesPrimeSieve implements PrimeSieve{
+    private int limit;
+    private List<Integer> numberList;
+
+    public EratosthenesPrimeSieve(int limit) {
+        this.limit = limit;
+
+    }
 
     @Override
     public boolean isPrime(int p) {
